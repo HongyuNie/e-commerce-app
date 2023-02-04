b4747ed8c4a15e43c8bc508c6d888617a5464ada
HongyuNie <nhy0613@outlook.com>
2023-02-03 19:42:42 -0800

    apply middleware globally

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+//pass a middleware function, let all the route handlders have it applied
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send(`
         <div>
@@ -16,7 +19,7 @@ app.get('/', (req, res) => {
     `);
 });
 
-app.post('/', bodyParser.urlencoded({extended: true}), (req, res) => {
+app.post('/', (req, res) => {
     console.log(req.body);
     res.send('Account created.');
 });
